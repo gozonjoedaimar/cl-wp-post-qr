319a2c7a1d271f457479c15da14f60c3691aab3c
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-11-12 13:56:01 +0000

    WP post qr route

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Post QR
+Description: Wp post qr plugin
+Version: 1.0
+Author: Joedaimar G.
+*/
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__ . '/src/qr_route.php';
---
+++ src/qr_route.php
@@ -0,0 +1,71 @@
+<?php
+
+use SimpleSoftwareIO\QrCode\Generator;
+
+if ( ! function_exists('cl_qr_route') ) {
+   /**
+    * QR code route
+    * 
+    * usage: /cl-post-qr/{post_id}
+    */
+    function cl_qr_route() {
+        // Your code here
+        $request_uri = $_SERVER['REQUEST_URI'];
+        $paths = explode('/', $request_uri);
+        $post_id = array_pop($paths);
+        $page = array_pop($paths);
+
+        $as_png = extension_loaded('imagick');
+    
+        if ($page === 'cl-post-qr') {
+            $link = cl_get_post_link($post_id);
+
+            if (empty($link)) {
+               status_header(404);
+               nocache_headers();
+               return;
+            }
+
+            // Default format
+            $ext = "svg";
+            $content_type = "image/svg+xml";
+
+            // Build QR code
+            $qr_generator = new Generator;
+            $qr = $qr_generator->size(200);
+
+            if ($as_png) {
+               $ext = "png";
+               $content_type = "image/png";
+               $qr = $qr->format('png');
+            }
+
+            // Generate QR code
+            $qr = $qr->generate($link);
+
+            // Set headers
+            header('Content-Type: '.$content_type);
+
+            if (isset($_GET['attachment'])) {
+               header('Content-Disposition: attachment; filename="qr.'.$ext.'"');
+            }
+
+            echo $qr;
+            die();
+        }
+    }
+    add_action('parse_query', 'cl_qr_route');
+}
+
+if ( ! function_exists('cl_get_post_link') ) {
+   /**
+    * Get post link
+    */
+   function cl_get_post_link($post_id) {
+      $post = get_post($post_id);
+      if ($post) {
+         return get_permalink($post);
+      }
+      return "";
+   }
+}
